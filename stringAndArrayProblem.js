fc8050e033b40e74f5c49d9e318b330e014833de
NeMa42 <60357016+NeMa42@users.noreply.github.com>
2020-02-19 22:38:04 +0400

    Update stringAndArrayProblem.js

--- stringAndArrayProblem.js
+++ stringAndArrayProblem.js
@@ -1,20 +1,15 @@
 function strAndArr(str, arr){
 let m = 0;
-let newStr = [];
-let strArr = str.split(""); 
 
+let strArr = str.split(""); 
 for( let i = 0; i< strArr.length; i++){
 	if(strArr[i] == "_"){
 		strArr[i] = arr[m];
 		m++;
-		newStr.push(strArr[i]);
-	}else{
-		newStr.push(strArr[i]);
 	}
 }
-let result = newStr.join("");
-console.log(result);
-
+let result = strArr.join("")
+console.log(result)
 }
 
 strAndArr("_, we have a _.", ["Houston", "problem"])
